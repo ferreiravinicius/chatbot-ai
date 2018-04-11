f5942b322a0610718f6a143a24ccb40b9937d0d3
Vinícius Ferreira <vinidotnet@gmail.com>
2018-04-11 19:34:37 -0300

    Adicionado trainer de lista para apresentacao

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-alfonse = ChatBot("Alfonse", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+alfonse = ChatBot(
+    "Alfonse", 
+    trainer="chatterbot.trainers.ListTrainer",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter"
+    )
 
-alfonse.set_trainer(ChatterBotCorpusTrainer)
-alfonse.train("chatterbot.corpus.portuguese")
+alfonse.train([
+    "Qual é a primeira lei de Newton?",
+    "Lei da Inércia, diz que a tendência dos corpos, quando nenhuma força é exercida sobre eles, é permanecer em seu estado natural, ou seja, repouso ou movimento retilíneo e uniforme. É por isso que quando o motorista freia o ônibus bruscamente e você está em pé, você quase cai no colo dele.",
+    "Quem é Isaac Newton?",
+    "Isaac Newton foi um cientista inglês do séc. XVII que formulou as leis da mecânica clássica.",
+    "Qual é a segunda lei de Newton?",
+    "A Segunda Lei de Newton diz que a força resultante que age sobre um corpo deve ser igual ao produto da massa do corpo por sua aceleração. De acordo com a Segunda Lei de Newton: “A força resultante que atua sobre um corpo é proporcional ao produto da massa pela aceleração por ele adquirida. ” É por isso que o Indiana Jones sai correndo para não ser esmagado pela pedrona."
+])
+
+#alfonse.set_trainer(ChatterBotCorpusTrainer)
+#alfonse.train("chatterbot.corpus.portuguese")
 
 
 @app.route("/")
@@ -21,4 +34,4 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
