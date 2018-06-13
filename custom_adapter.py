dc8442100a92ac3746c5e7395accfbca75b8b95a
Vinícius Ferreira <vinidotnet@gmail.com>
2018-06-13 03:09:46 -0300

    Adapter finalizado

---
+++ custom_adapter.py
@@ -0,0 +1,61 @@
+from chatterbot.logic import LogicAdapter
+from chatterbot.conversation import Statement
+from unidecode import unidecode
+import secrets
+
+intents = {
+    'primeira_lei_newton' : {
+        'required': ['primeira', 'lei', 'newton'],
+        'optional': [],
+        'answer': "teste_newton_1"
+    },
+    'segunda_lei_newton' : {
+        'required': ['segunda', 'lei', 'newton'],
+        'optional': [],
+        'answer': "teste_newton_2"
+    },
+    'segunda_lei_kepler' : {
+        'required': ['segunda', 'lei', 'kepler'],
+        'optional': [],
+        'answer': "teste_kepler_2"
+    },
+}
+
+class CustomAdapter(LogicAdapter):
+    def __init__(self, **kwargs):
+        super(CustomAdapter, self).__init__(**kwargs)
+
+    def can_process(self, statement):
+        return True
+
+    def process(self, statement):
+        text = self.sanitize(statement.text)
+
+        for intent, data in intents.items():
+            if all(self.match(text, word) for word in data.get('required')):
+                return Statement(data.get('answer'))
+
+        random_text = secrets.choice([
+            "Não consegui encontrar sobre este assunto. Tente ser mais específico citando o nome do cientista e a teoria que deseja.",
+            "Não consegui entender, parece incompleto. Tente acrescentar o nome do cientista.",
+            "Você precisa ser um pouco mais epecífico, talvez dizer o nome do cientista junto com a teoria ajude."
+        ])
+
+        response = Statement(random_text)
+        response.confidence = 1
+        return response
+
+    def sanitize(self, text):
+        return unidecode(text.lower())
+
+    def match(self, text, word):
+        import re
+        return word in re.split(' |-', text)
+
+
+        #points = {}
+        # for intent, data in intents.items():
+        #     for word in data.get('required'):
+        #         if match(stm, word):
+        #            points.update({intent, points.get(intent, default=0) + 1}) 
+
